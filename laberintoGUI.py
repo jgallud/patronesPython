1694ae6d2fe62b40a914e58271fd90fe2a9d2d7d
Jose A. Gallud <jose.gallud@uclm.es>
2018-04-27 12:19:17 +0200

    dibujar laberinto

--- laberintoGUI.py
+++ laberintoGUI.py
@@ -1,33 +1,112 @@
 import os
 import wx
-from laberintoBuilder import * 
+from laberintoBuilder import *
 
 class MainWindow(wx.Frame):
-	def __init__(self, parent, title):
-	    wx.Frame.__init__(self, parent, title="LaberintoGUI", size=(300,300))
-	    panel = wx.Panel(self)
-	    self.quote = wx.StaticText(panel, label="Archivo: ", pos=(20, 30))  
-	    self.archivo = wx.TextCtrl(panel, value="Localiza el archivo", pos=(20,50),size=(150,20),style=wx.TE_READONLY)
-	    self.button =wx.Button(panel, label="Seleccionar y procesar", pos=(20, 80))
-	    wx.StaticText(panel, label="Numero de bichos: ", pos=(20, 130))
-	    self.bichos = wx.TextCtrl(panel, value="", pos=(20,150),size=(150,20),style=wx.TE_READONLY)
-	    self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)      
-	    self.Show()
-	def OnClick(self,event):        
-	    self.dirname = ""
-	    dlg = wx.FileDialog(self, "Selecciona un archivo", self.dirname, "", "*.json", wx.FD_OPEN)
-	    if dlg.ShowModal() == wx.ID_OK:
-	        self.filename = dlg.GetFilename()
-	        self.dirname = dlg.GetDirectory()
-	        #f = open(os.path.join(self.dirname, self.filename), 'r')
-	        #self.control.SetValue(f.read())
-	        self.archivo.SetValue(self.filename)
-	        #f.close()
-	        director=Director()	        
-	        director.procesar(os.path.join(self.dirname, self.filename))
-	        self.juego=director.builder.juego
-	        self.bichos.SetValue(str(len(self.juego.bichos)))
-	    dlg.Destroy()
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title="LaberintoGUI", size=(800,600))
+        self.origen=wx.Point(350,14)
+        self.ancho=None
+        self.alto=None
+        self.puedeDibujar=False
+        self.dc=None
+        panel = wx.Panel(self)
+        self.quote = wx.StaticText(panel, label="Archivo: ", pos=(20, 30))
+        self.archivo = wx.TextCtrl(panel, value="Localiza el archivo", pos=(20,50),size=(150,20),style=wx.TE_READONLY)
+        self.button =wx.Button(panel, label="Seleccionar y procesar", pos=(20, 80))
+        self.bichos = wx.TextCtrl(panel, value="", pos=(20,150),size=(150,20),style=wx.TE_READONLY)
+        self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
+        self.Bind(wx.EVT_PAINT,self.on_paint)
+        self.Show()
+
+    def on_paint(self,event=None):
+        self.dc=wx.PaintDC(self)
+        self.dc.Clear()
+        self.dc.SetPen(wx.Pen("BLACK",3))
+        #self.dc.DrawLine(300,0,350,50)
+        if (self.puedeDibujar):
+            self.dibujarLaberinto()
+
+    def OnClick(self,event):
+        self.dirname = ""
+        dlg = wx.FileDialog(self, "Selecciona un archivo", self.dirname, "", "*.json", wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            #f = open(os.path.join(self.dirname, self.filename), 'r')
+            #self.control.SetValue(f.read())
+            self.archivo.SetValue(self.filename)
+            #f.close()
+            director=Director()
+            director.procesar(os.path.join(self.dirname, self.filename))
+            self.juego=director.builder.juego
+            self.bichos.SetValue(str(len(self.juego.bichos)))
+            self.mostrarLaberinto()
+        dlg.Destroy()
+
+    def mostrarLaberinto(self):
+        self.calcularPosiciones()
+        self.normalizar()
+        self.calcularDimensiones()
+        self.asignarPuntosReales()
+        self.puedeDibujar=True
+        self.Refresh()
+        self.Update()
+        #self.dibujarLaberinto()
+
+    def calcularPosiciones(self):
+        if (self.juego!=None):
+            h=self.juego.obtenerHabitacion(1)
+            h.punto=wx.Point(0,0)
+            h.calcularPosicion()
+
+    def normalizar(self):
+        minX=0
+        minY=0
+        for h in self.juego.laberinto.hijos:
+            if (h.punto.x<minX):
+                minX=h.punto.x
+            if (h.punto.y<minY):
+                minY=h.punto.y
+        for h in self.juego.laberinto.hijos:
+            punto=h.punto
+            h.punto.x=punto.x+abs(minX)
+            h.punto.y=punto.y+abs(minY)
+
+    def calcularDimensiones(self):
+        maxX=0
+        maxY=0
+        self.origen=wx.Point(350,14)
+        for h in self.juego.laberinto.hijos:
+            if (h.punto.x>maxX):
+                maxX=h.punto.x
+            if (h.punto.y>maxY):
+                maxY=h.punto.y
+        maxX=maxX+1
+        maxY=maxY+1
+        self.ancho=550/maxX
+        self.alto=500/maxY
+
+    def asignarPuntosReales(self):
+        for h in self.juego.laberinto.hijos:
+            x=self.origen.x+(h.punto.x * self.ancho)
+            y=self.origen.y+(h.punto.y * self.alto)
+            h.punto=wx.Point(x,y)
+            h.extent=wx.Point(self.ancho,self.alto)
+            for ho in h.hijos:
+                ho.asignarPuntosReales(h)
+
+    def dibujarLaberinto(self):
+        for h in self.juego.laberinto.hijos:
+            h.dibujar(self)
+
+    def dibujarContenedorRectangular(self,unCont):
+        x1=unCont.punto.x
+        y1=unCont.punto.y
+        x2=unCont.extent.x
+        y2=unCont.extent.y
+        #print(x1,y1,x2,y2)
+        self.dc.DrawRectangle(x1,y1,x2,y2)
 
 app = wx.App(False)
 frame = MainWindow(None, "Sample editor")
--- solucionLaberinto.py
+++ solucionLaberinto.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python
 import time
+import wx
 
 class JuegoLaberinto:
- def __init__(self):
-   	self.laberinto=None
-   	self.bichos=list()
- def crearLaberinto2Hab(self,unAF):
-	 norte = Norte()
-	 sur = Sur()
-	 este = Este()
-	 oeste = Oeste()
-	 lab = unAF.fabricarLaberinto()
-	 hab1 = unAF.fabricarHabitacion(1)
-	 hab2 = unAF.fabricarHabitacion(2)
-	 pt = unAF.fabricarPuerta(hab1, hab2)
-	 hab1.ponerEn(norte,pt)
-	 hab2.ponerEn(sur,pt)
-	 hab1.ponerEn(este, unAF.fabricarPared())
-	 hab1.ponerEn(oeste, unAF.fabricarPared())
-	 hab1.ponerEn(sur, unAF.fabricarPared())
-	 hab2.ponerEn(este, unAF.fabricarPared())
-	 hab2.ponerEn(oeste, unAF.fabricarPared())
-	 hab2.ponerEn(norte, unAF.fabricarPared())
-	 lab.agregarHijo(hab1)
-	 lab.agregarHijo(hab2)
-	 self.laberinto=lab
+	def __init__(self):
+		self.laberinto=None
+		self.bichos=list()
+	def crearLaberinto2Hab(self,unAF):
+		norte = Norte()
+		sur = Sur()
+		este = Este()
+		oeste = Oeste()
+		lab = unAF.fabricarLaberinto()
+		hab1 = unAF.fabricarHabitacion(1)
+		hab2 = unAF.fabricarHabitacion(2)
+		pt = unAF.fabricarPuerta(hab1, hab2)
+		hab1.ponerEn(norte,pt)
+		hab2.ponerEn(sur,pt)
+		hab1.ponerEn(este, unAF.fabricarPared())
+		hab1.ponerEn(oeste, unAF.fabricarPared())
+		hab1.ponerEn(sur, unAF.fabricarPared())
+		hab2.ponerEn(este, unAF.fabricarPared())
+		hab2.ponerEn(oeste, unAF.fabricarPared())
+		hab2.ponerEn(norte, unAF.fabricarPared())
+		lab.agregarHijo(hab1)
+		lab.agregarHijo(hab2)
+		self.laberinto=lab
 
- def obtenerHabitacion(self,id):
-   return self.laberinto.obtenerHabitacion(id)
+	def obtenerHabitacion(self,id):
+		return self.laberinto.obtenerHabitacion(id)
 
- def agregarBicho(self,unBicho):
-	 self.bichos.append(unBicho)
+	def agregarBicho(self,unBicho):
+		self.bichos.append(unBicho)
 
 class ElementoMapa:
 	def entrar(self):
@@ -51,11 +52,17 @@ class ElementoMapa:
 		pass
 	def __repr__(self):
 		pass
+	def asignarPuntosReales(self,unCont):
+		pass
+	def dibujar(self):
+		pass
 
 class Contenedor(ElementoMapa):
 	def __init__(self):
 		self.hijos=list()
 		self.forma=None
+		self.punto=None
+		self.extent=None
 	def agregarHijo(self,unEM):
 		self.hijos.append(unEM)
 	def eliminarHijo(self,unEM):
@@ -63,15 +70,30 @@ class Contenedor(ElementoMapa):
 	def ponerEn(self,unaOr,unEM):
 		self.forma.ponerEn(unaOr,unEM)
 	def enumerar(self):
-		print repr(self)
+		print(repr(self))
 		for e in self.hijos:
 			e.enumerar()
+	def calcularPosicion(self):
+		self.forma.calcularPosicionDesde(self)
+	def asignarPuntosReales(self,unCont):
+		self.forma.asignarPuntosRealesDeEn(unCont,self)
+	def dibujar(self,unaVista):
+		self.forma.dibujarContenedor(unaVista,self)
+		for h in self.hijos:
+			h.dibujar(unaVista)
 
 class Forma:
 	def __init__(self):
 		self.orientaciones=list()
 	def ponerEn(self,unaOr,unEM):
 		unaOr.poner(unEM,self)
+	def calcularPosicionDesde(self,unCont):
+		for ori in self.orientaciones:
+			ori.calcularPosicionDesde(unCont)
+	def asignarPuntosRealesDeEn(self,unCont,otroCont):
+		pass
+	def dibujarContenedor(self,unaVista,unCont):
+		pass
 
 class Cuadrado(Forma):
 	def __init__(self):
@@ -80,6 +102,20 @@ class Cuadrado(Forma):
 		self.sur=None
 		self.oeste=None
 		Forma.__init__(self)
+
+	def asignarPuntosRealesDeEn(self,unCont,otroCont):
+		ancho=(unCont.extent.x) * otroCont.proporcion
+		alto = (unCont.extent.y) * otroCont.proporcion
+		#print("ancho,alto",ancho,alto)
+		division=ancho/len(unCont.hijos)
+		numHijo=unCont.hijos.index(otroCont)
+		x=unCont.punto.x+(division * numHijo)
+		y=unCont.punto.y + (unCont.extent.y - alto)
+		otroCont.punto=wx.Point(x,y)
+		otroCont.extent=wx.Point(ancho,alto)
+
+	def dibujarContenedor(self,unaVista,unCont):
+		unaVista.dibujarContenedorRectangular(unCont)
 
 class Octogono(Forma):
 	def __init__(self):
@@ -98,7 +134,7 @@ class Laberinto(Contenedor):
 		for item in self.hijos:
 			if item.esHabitacion and item.id==id:
 				return item
-		print "no encontrado"
+		print("no encontrado")
 	def __repr__(self):
 		return "Laberinto"
 
@@ -109,35 +145,47 @@ class Habitacion(Contenedor):
 	def __repr__(self):
 		return "Habitacion"
 	def entrar(self):
-		print "Estas en la habitacion-"+repr(self.id)
+		print("Estas en la habitacion-"+repr(self.id))
 	def esHabitacion(self):
 		return True
 
 class Hoja(ElementoMapa):
 	def entrar(self):
-		print "es una hoja"
+		print("es una hoja")
 	def enumerar(self):
-		print repr(self)
+		print(repr(self))
+	def calcularPosicionDesdePunto(self,unCont,punto):
+		pass
+	def asignarPuntosReales(self, unCont):
+		pass
+	def dibujar(self,unaVista):
+		pass
 
 class Armario(Contenedor):
+	def __init__(self):
+		self.proporcion=0.3
+		Contenedor.__init__(self)
 	def esArmario(self):
 		return True
 	def entrar(self):
-		print "estas en un armario"
+		print("estas en un armario")
 	def __repr__(self):
 		return "Armario"
 
 class Baul(Contenedor):
+	def __init__(self):
+		self.proporcion=0.1
+		Contenedor.__init__(self)
 	def esBaul(self):
 		return True
 	def entrar(self):
-		print "estas en un baul"
+		print("estas en un baul")
 	def __repr__(self):
 		return "Baul"
 
 class Pared(Hoja):
 	def entrar(self):
-		print "Te has chocado con una pared"
+		print("Te has chocado con una pared")
 	def esPared(self):
 		return True
 	def __repr__(self):
@@ -148,15 +196,25 @@ class Puerta(Hoja):
 		self.h1=h1
 		self.h2=h2
 		self.abierta=False
+		self.visitada=False
 	def entrar(self):
 		if self.abierta:
-			print "Puerta abierta"
+			print("Puerta abierta")
 		else:
-			print "Puerta cerrada"
+			print("Puerta cerrada")
 	def esPuerta(self):
 		return True
 	def __repr__(self):
 		return "Puerta"
+	def calcularPosicionDesdePunto(self,unCont,punto):
+		if (not self.visitada):
+			self.visitada=True
+			if (unCont.id==self.h1.id):
+				self.h2.punto=punto
+				self.h2.calcularPosicion()
+			else:
+				self.h1.punto=punto
+				self.h1.calcularPosicion()
 
 class Decorator(Hoja):
 	def __init__(self):
@@ -180,7 +238,7 @@ class Estrategia:
 		else:
 			bomba.componente.entrar()
 	def imprimir(self):
-		print "sobreescribir mensaje"
+		print("sobreescribir mensaje")
 	def esBroma(self):
 		return False
 	def esMina(self):
@@ -190,38 +248,60 @@ class Estrategia:
 
 class Broma(Estrategia):
 	def imprimir(self):
-		print "bomba broma"
+		print("bomba broma")
 	def esBroma(self):
 		return True
 
 class H(Estrategia):
 	def imprimir(self):
-		print "bomba H"
+		print("bomba H")
 	def esH(self):
 		return True
 
 class Mina(Estrategia):
 	def imprimir(self):
-		print "bomba mina"
+		print("bomba mina")
 	def esMina(self):
 		return True
 
 class Orientacion:
 	def poner(self,elemento,forma):
 		pass
+	def calcularPosicionDesde(self,unCont):
+		pass
 
 class Norte(Orientacion):
 	def poner(self,elemento,forma):
 		forma.norte=elemento
+	def calcularPosicionDesde(self,unCont):
+		x=unCont.punto.x
+		y=unCont.punto.y-1
+		punto=wx.Point(x,y)
+		unCont.forma.norte.calcularPosicionDesdePunto(unCont,punto)
 class Sur(Orientacion):
 	def poner(self,elemento,forma):
 		forma.sur=elemento
+	def calcularPosicionDesde(self,unCont):
+		x=unCont.punto.x
+		y=unCont.punto.y+1
+		punto=wx.Point(x,y)
+		unCont.forma.sur.calcularPosicionDesdePunto(unCont,punto)
 class Este(Orientacion):
 	def poner(self,elemento,forma):
 		forma.este=elemento
+	def calcularPosicionDesde(self,unCont):
+		x=unCont.punto.x+1
+		y=unCont.punto.y
+		punto=wx.Point(x,y)
+		unCont.forma.este.calcularPosicionDesdePunto(unCont,punto)
 class Oeste(Orientacion):
 	def poner(self,elemento,forma):
 		forma.oeste=elemento
+	def calcularPosicionDesde(self,unCont):
+		x=unCont.punto.x-1
+		y=unCont.punto.y
+		punto=wx.Point(x,y)
+		unCont.forma.oeste.calcularPosicionDesdePunto(unCont,punto)
 class NorOeste(Orientacion):
 	def poner(self,elemento,forma):
 		forma.noroeste=elemento
